028b7454bb3b6f6194a3ec05afc86ae79e21cd0e
Burak Aykan Ürer <burakaykan2@gmail.com>
2019-01-30 22:58:52 +0300

    Chapter 2.4 Added:
    Configuring custom interceptors

--- src/main/java/com/test/bookpub/WebConfiguration.java
+++ src/main/java/com/test/bookpub/WebConfiguration.java
@@ -3,11 +3,25 @@ package com.test.bookpub;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
 
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter{
     @Bean
     public RemoteIpFilter remoteIpFilter(){
         return new RemoteIpFilter();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
