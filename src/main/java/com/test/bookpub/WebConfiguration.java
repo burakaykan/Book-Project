5c9fe6e66496b3092516e500374240e6238da13c
Burak Aykan Ürer <burakaykan2@gmail.com>
2019-01-31 00:02:23 +0300

    Chapter 2.5 Added:
    Configuring custom HttpMessageConverters

--- src/main/java/com/test/bookpub/WebConfiguration.java
+++ src/main/java/com/test/bookpub/WebConfiguration.java
@@ -3,9 +3,13 @@ package com.test.bookpub;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.List;
 
 
 @Configuration
@@ -24,4 +28,23 @@ public class WebConfiguration extends WebMvcConfigurerAdapter{
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
     }
+
+    //Bean olarak HttpMessageConverter tanımlamak en basit ve en hızlı yol
+    @Bean
+    public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter(){
+        return new ByteArrayHttpMessageConverter();
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
+        converters.add(new ByteArrayHttpMessageConverter());
+    }
+
+    //Biraz daha fazla kontrol sağlar
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        converters.clear();
+        converters.add(new ByteArrayHttpMessageConverter());
+    }
+
 }
