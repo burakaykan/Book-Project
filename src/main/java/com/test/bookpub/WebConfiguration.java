60dee0c47476d2ac6a80e9961f3c6dc49530228b
Burak Aykan Ürer <burakaykan2@gmail.com>
2019-01-30 20:21:31 +0300

    Chapter 2.3 Added:
    Configuring custom servlet filters (with Problems)

---
+++ src/main/java/com/test/bookpub/WebConfiguration.java
@@ -0,0 +1,13 @@
+package com.test.bookpub;
+
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+    @Bean
+    public RemoteIpFilter remoteIpFilter(){
+        return new RemoteIpFilter();
+    }
+}
