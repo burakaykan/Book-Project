2067f8f5ace16bbfe6ea5c68b2e2acf8ccc8ea28
Burak Aykan Ürer <burakaykan2@gmail.com>
2019-01-30 17:46:08 +0300

    Chapter 1 - Complated
    In this chapter
    
    Using a Spring Boot template and starter
    Creating a simple application
    Launching an application using Gradle
    Using the command-line runners
    Setting up a database connection
    Setting up a data repository service
    Scheduling executors
    
    Complated.

--- src/main/java/com/test/bookpub/StartupRunner.java
+++ src/main/java/com/test/bookpub/StartupRunner.java
@@ -5,17 +5,24 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.sql.DataSource;
 
 
-public class StartupRunner implements CommandLineRunner{
+public class StartupRunner implements CommandLineRunner {
     protected final Log logger = LogFactory.getLog(getClass());
 
     @Autowired
     private BookRepository bookRepository;
 
-    public void run(String... args) throws Exception{
+    public void run(String... args) throws Exception {
+        //logger.info("Number of books: " + bookRepository.count());
+    }
+
+    //Planlamış olarak yapmamızı sağlıyor bu annotation bunu kullanabilmek için @EnabledScheduled ı eklememiz gerekiyor bunu BookPubApplication.java da ekledik
+    @Scheduled(initialDelay = 1000, fixedRate = 10000)
+    public void run() {
         logger.info("Number of books: " + bookRepository.count());
     }
 }
